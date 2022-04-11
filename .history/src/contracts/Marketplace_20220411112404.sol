9e0d5bca2a27facacc9c56ff917e094541d43929
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-04-11 11:50:05 -0400

    test written

---
+++ .history/src/contracts/Marketplace_20220411112404.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract Marketplace {
+    string public name;
+
+    constructor() public {
+        name = "Didier Test Marketplace";
+    }
+}
